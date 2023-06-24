cc3f226b8cb4055e95675fd2a050dc61b7e2ba0a
KateM27 <mamocatherine@gmail.com>
2023-06-24 22:42:56 +0300

    using wrong var

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf8');
-
 process.stdin.on('readable', () => {
-  const input = process.stdin.read();
+  const chunk = process.stdin.read();
 
-  if (input) {
+  if (chunk) {
     process.stdout.write(`Your name is: ${input}`);
   }
 });
